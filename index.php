d52facef3e69be46674541609ba5c6d3832a66e0
Stizwesa <blaynstigger@gmail.com>
2025-06-22 18:59:55 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>
+assignment
+</title>
+<body>
+<div style="background-color: lightblue; padding: 20px">
+  <h1 style="background-color: darkblue; color:white; padding: 15px">About Me - Alex Johnson</h1>
+    <p>Hi there! My name is Alex and I'm a
+        high school student who loves
+        technology and sports. I enjoy
+        learning new things, especially about
+        computers and programming. In my free time,
+        I like to play basketball and hang out with my
+        friends.</p>
+   <h2 style="background-color: lightgreen; padding: 10px;">My Favorites</h2>
+   <table style="background-color: white; padding: 10px; width:100% ">
+       <thead>
+       <tr style="background-color: gray;color: white;">
+          <th style="padding: 8px">Category</th>
+           <th style="padding: 8px">Item</th>
+           <th style="padding: 8px">Why I like it</th>
+       </tr>
+       </thead>
+       <tbody>
+       <tr style="background-color: lightyellow; padding: 8px">
+           <td style="padding: 8px">Food</td>
+           <td style="padding: 9px">Pizza</td>
+           <td style="padding: 8px">It's delicious and perfect for sharing</td>
+       </tr>
+       <tr style="background-color: lightpink; padding: 8px">
+           <td style="padding: 8px">Movie</td>
+           <td style="padding: 9px">Spider-Man</td>
+           <td style="padding: 8px">Amazing Story and great action</td>
+       </tr>
+       <tr style="background-color: lightyellow; padding: 8px">
+           <td style="padding: 8px">Sport</td>
+           <td style="padding: 9px">BasketBall</td>
+           <td style="padding: 8px">Fast-paced and fun to play with friends</td>
+       </tr>
+       <tr style="background-color: lightpink; padding: 8px">
+           <td style="padding: 8px">Subject</td>
+           <td style="padding: 9px">Computer Science</td>
+           <td style="padding: 8px">I love solving problems with code</td>
+       </tr>
+       </tbody>
+   </table>
+    <h2 style="background-color:orange;padding: 10px;">My Weekly Schedule</h2>
+    <table style="background-color: white; width: 100%;padding: 15px">
+       <thead>
+       <tr style="background-color: purple; ">
+           <th style="padding: 10px; color: white">Day</th>
+           <th style="padding: 10px; color: white">Morning</th>
+           <th style="padding: 10px; color: white">Afternoon</th>
+           <th style="padding: 10px; color: white">Evening</th>
+       </tr>
+       </thead>
+        <tbody>
+        <tr>
+            <td style="background-color: lightcyan;padding: 10px">Monday</td>
+            <td style="background-color: lightcyan; padding:10px">School</td>
+            <td style="background-color: lightcyan; padding:10px">Basketball pratice</td>
+            <td style="background-color: lightcyan;padding:10px">Homework</td>
+        </tr>
+        <tr>
+            <td style="background-color: lightgray;padding: 10px">Wednesday</td>
+            <td style="background-color: lightgray; padding:10px">School</td>
+            <td style="background-color: lightgray; padding:10px">Computer club</td>
+            <td style="background-color: lightgray;padding:10px">Family Time</td>
+        </tr>
+        <tr>
+            <td style="background-color: lightcyan;padding: 10px">Saturday</td>
+            <td style="background-color: lightcyan; padding:10px">Sleep in!</td>
+            <td style="background-color: lightcyan; padding:10px">Basketball game</td>
+            <td style="background-color: lightcyan;padding:10px">Friends hangout</td>
+        </tr>
+        </tbody>
+    </table>
+    <h2>My Morning Routine</h2>
+     <div style="background-color: lightyellow;padding: 10px">
+         <ol>
+             <li>Wake up at 6:30am</li>
+             <li>Brush my teeth and wash my face</li>
+             <li>Eat breakfast (usually cereal)</li>
+             <li>Get dressed for school</li>
+             <li>Pack my backpack with homework</li>
+             <li>walk to the bus stop</li>
+         </ol>
+     </div>
+    <br>
+    <div style="background-color: lightcoral; padding: 15px">
+        <h2>Websites I use</h2>
+        <p>I spend a lot of time online for both school
+        and fun. I use <a href="https://google.com"> Google</a>for
+            research and homework help. <a href="https://youtube.com"> Youtube</a>is great
+            for learning new programming tutorials. I also check <a href="https://github.com">Github</a>
+            to see cool coding projects from other developers.
+        </p>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Stizwesa/assignment-.git
+git push -u origin main

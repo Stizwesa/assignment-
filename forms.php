8ac59f667f8f233f9fbb141c0da942097139c2f0
Stizwesa <blaynstigger@gmail.com>
2025-06-22 22:04:00 +0000

    Add files via upload

---
+++ forms.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>assignment</title>
+<body>
+<h1>Forms</h1>
+
+
+<form>
+    <label for="first_name">First Name</label>
+    <input type="text" id="first_name" name="first_name" required readonly>
+    <br>
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" required>
+    <br>
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <label for="age">Age</label>
+    <input type="number" id="age" name="age" required>
+    <br>
+    <label for="">Contact</label>
+    <input type="tel" id="contact" name="contact"> <br>
+
+    <label for="dob">Date of birth</label>
+    <input type="date" id="dob" name="dob"> <br>
+
+    <input type="submit">
+
+
+</form>
+
+
+
+
+
+</body>
+</html>
+
